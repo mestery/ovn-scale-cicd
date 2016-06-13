5ad9c546fb9f9ea44a3b1aac2d663f9c216b9d7d
Kyle Mestery <mestery@mestery.com>
2016-06-13 17:56:38 +0000

    Run additional tests
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scale-run.sh
+++ scale-run.sh
@@ -30,9 +30,25 @@ popd
 docker exec ovn-rally rally-ovs deployment create --file /root/rally-ovn/ovn-multihost-deployment.json --name ovn-multihost
 
 # Register the emulated sandboxes in the rally database
-docker exec ovn-rally rally-ovs task start /root/rally-ovn/workload/create-sandbox-ovn-rally.json
+docker exec ovn-rally rally-ovs task start /root/rally-ovn/workload/create-sandbox-$OVN_RALLY_HOSTNAME.json
+TASKID=$(docker exec ovn-rally rally task list --uuids-only)
+docker exec ovn-rally rally task report $TASKID --out /root/create-sandbox-output.html
+docker exec ovn-rally rally task delete --uuid $TASKID
 
 # Run tests
 docker exec ovn-rally rally-ovs task start /root/rally-ovn/workload/create_networks.json
+TASKID=$(docker exec ovn-rally rally task list --uuids-only)
+docker exec ovn-rally rally task report $TASKID --out /root/create-networks-output.html
+docker exec ovn-rally rally task delete --uuid $TASKID
+
+docker exec ovn-rally rally-ovs task start /root/rally-ovn/workload/create_and_list_lports.json
+TASKID=$(docker exec ovn-rally rally task list --uuids-only)
+docker exec ovn-rally rally task report $TASKID --out /root/create-and-list-lports-output.html
+docker exec ovn-rally rally task delete --uuid $TASKID
+
+docker exec ovn-rally rally-ovs task start /root/rally-ovn/workload/create_and_bind_ports.json
+TASKID=$(docker exec ovn-rally rally task list --uuids-only)
+docker exec ovn-rally rally task report $TASKID --out /root/create-and-bind-ports-output.html
+docker exec ovn-rally rally task delete --uuid $TASKID
 
 popd
